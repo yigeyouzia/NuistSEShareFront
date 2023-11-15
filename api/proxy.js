9d62494ecf0253499a54c3c56068cedc81320bc6
cyt <1793402316@qq.com>
2023-11-15 23:07:03 +0800

    vecel1

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
   if (req.url.startsWith('/api')) { // 这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
-    target = 'https://share.mypikpak.buzz' // 这里就是在vite中配置的一样
+    target = 'https://nuistshebackend-jpthigmgez.cn-hangzhou.fcapp.run' // 这里就是在vite中配置的一样
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
