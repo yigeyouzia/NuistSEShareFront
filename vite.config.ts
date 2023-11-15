d4bd16bf50e75010bf752152b1662ac706cdf563
cyt <1793402316@qq.com>
2023-11-15 23:27:42 +0800

    vecel4

--- vite.config.ts
+++ vite.config.ts
@@ -39,17 +39,17 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     // 代理跨域
-    // server: {
-    //   proxy: {
-    //     [env.VITE_APP_BASE_API]: {
-    //       // 获取数据的服务器地址设置
-    //       target: env.VITE_SERVE,
-    //       // 需要代理跨域
-    //       changeOrigin: true,
-    //       // 路径重写
-    //       rewrite: path => path.replace(/^\/api/, ''),
-    //     },
-    //   },
-    // },
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          // 获取数据的服务器地址设置
+          target: env.VITE_SERVE,
+          // 需要代理跨域
+          changeOrigin: true,
+          // 路径重写
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
   }
 })
